84d64da3e6117b303c511e08c2f2c2a1d9193447
Chase Dodge <75660257+Chas-w@users.noreply.github.com>
2025-02-03 17:27:46 -0500

    Navmesh fix - enemy bug no longer making me angry

--- Assets/Scripts/Enemy/New Enemy Logic - Chase/eManager.cs
+++ Assets/Scripts/Enemy/New Enemy Logic - Chase/eManager.cs
@@ -22,6 +22,7 @@ public class eManager : MonoBehaviour //don't forget to change the script name i
     public bool died;
     public bool attacked;
 
+
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -41,7 +42,8 @@ public class eManager : MonoBehaviour //don't forget to change the script name i
                     agent.SetDestination(point);
                 }
             }
-        } else if (distanceCheck.chase)
+        } 
+        if (distanceCheck.chase)
         {
             if (agent.remainingDistance <= agent.stoppingDistance) //done with path
             {
@@ -58,7 +60,7 @@ public class eManager : MonoBehaviour //don't forget to change the script name i
 
         Vector3 randomPoint = center + Random.insideUnitSphere * range; //random point in a sphere 
         NavMeshHit hit;
-        if (NavMesh.SamplePosition(randomPoint, out hit, 5f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
+        if (NavMesh.SamplePosition(randomPoint, out hit, 1f, NavMesh.AllAreas)) //documentation: https://docs.unity3d.com/ScriptReference/AI.NavMesh.SamplePosition.html
         {
             //the 1.0f is the max distance from the random point to a point on the navmesh, might want to increase if range is big
             //or add a for loop like in the documentation
